528a06253c8c471d9d3aeab539f80bda444ccf01
dvillas28 <dvillasc@gmail.com>
2024-05-21 00:39:47 -0400

    restaurantes tabla

---
+++ backend/cargador_restaurante.py
@@ -0,0 +1,46 @@
+import psycopg2 as psy2
+import params as p
+import csv
+
+# cargar los datos brutos
+lineas = []
+with open('backend/data/restaurantes.csv', mode='r', encoding='mac_roman') as file:
+    reader = csv.reader(file, delimiter=';')
+    encabezado = next(reader)  # Saltar la fila de encabezado
+    for fila in reader:
+        lineas.append(fila)
+
+# quitamos las tuplas repetidas
+data_no_repetidos = []
+for fila in lineas:
+    dato = (fila[0], fila[1], fila[2], fila[3])
+    if dato not in data_no_repetidos:
+        data_no_repetidos.append(dato)
+
+
+conn = psy2.connect(**p.conn_params)
+cur = conn.cursor()
+
+insert_query = """
+    INSERT INTO restaurant (nombre, vigente, estilo, precio_min_reparto_gratis) VALUES (%s, %s, %s, %s);
+"""
+
+subidos = 0
+no_subidos = 0
+for dato in data_no_repetidos:
+    try:
+        cur.execute(
+            insert_query, dato)
+        conn.commit()
+        subidos += 1
+
+    except psy2.Error as e:
+        conn.rollback()
+        print(dato)
+        print(e)
+        no_subidos += 1
+
+print(
+    f'Se subieron {subidos} registros y no se subieron {no_subidos} registros: {no_subidos / subidos * 100:.2f}%')
+cur.close()
+conn.close()
